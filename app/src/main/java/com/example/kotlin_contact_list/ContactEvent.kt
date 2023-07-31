f2428595f028d69c237f848fc3f7883ec3ee9401
Arjun Suthaharan <arjunsutha@gmail.com>
2023-07-31 19:17:44 -0400

    Created ContactDatabase, ContactEvent and SortType enum for determining sort order type.

---
+++ app/src/main/java/com/example/kotlin_contact_list/ContactEvent.kt
@@ -0,0 +1,14 @@
+package com.example.kotlin_contact_list
+
+sealed interface ContactEvent{
+    object SaveContact: ContactEvent
+    data class SetFirstName(val firstName: String): ContactEvent
+    data class SetLastName(val lastName: String): ContactEvent
+    data class SetPhoneNum(val phoneNumber: String): ContactEvent
+    object ShowDialog: ContactEvent
+    object HideDialog: ContactEvent
+    data class SortContacts(val sortType: SortType): ContactEvent
+    data class DeleteContacts(val contact: Contact): ContactEvent
+
+
+}
---
+++ app/src/main/java/com/example/kotlin_contact_list/SortType.kt
@@ -0,0 +1,7 @@
+package com.example.kotlin_contact_list
+
+enum class SortType {
+    FIRST_NAME,
+    LAST_NAME,
+    PHONE_NUMBER
+}
